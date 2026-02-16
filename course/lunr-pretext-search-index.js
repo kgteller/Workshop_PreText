2a2e1cdeac9a078435ccfb1b1a1d3b54a720bfed
kgteller <132956779+kgteller@users.noreply.github.com>
2026-02-15 19:10:31 -0500

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -19,18 +19,18 @@ var ptx_lunr_docs = [
   "body": "course name (MATH xxx, section xxx) "
 },
 {
-  "id": "notes-week-01",
+  "id": "week01",
   "level": "1",
-  "url": "notes-week-01.html",
+  "url": "week01.html",
   "type": "Handout",
   "number": "",
   "title": "SLO 1: Sampling, Types of Data",
-  "body": " SLO 1: Sampling, Types of Data   This is an outline of the topics we covered in the first week of class.      Construct a simple random sample    Determine when samples of convenience are acceptable    Describe stratified sampling, cluster sampling, systematic sampling, and voluntary response sampling    Distinguish between statistics and parameters      1 Background and Definitions  In the months leading up to an election, polls often tell us the percentages of voters that prefer each of the candidates. How do pollsters obtain this information?   Ideal case: ask the opinion of every registered voter.   Workable: contact a relatively small number of voters, and use the information from these voters to predict the preferences of the entire group of voters.  The process of polling requires two major steps:                Statsitics is the study of       The polling problem is typical of a problem in statistics. We want some information about a large group of individuals, but we are able to collect information on only a small part of that group.  In statistical terminology, the large group is called a , and the part of the group on which we collect information is called a     A population is the of individuals about which information is sought. A sample is , containing the individuals that are actually observed.         Simple Random Sampling \\   A Simple Random Sample of size n is a sample chosen by a method in which collection of n population items is  to make up the sample just as in     Construct a simple random sample:  Using a computer random number generator. Suppose there are N items in the population. \\                   Example 1 Using technology draw a simple random sample of five employees from the following list   a table of names fro 40 employees at a company    "
+  "body": " SLO 1: Sampling, Types of Data   This is an outline of the topics we covered in the first week of class.      Construct a simple random sample    Determine when samples of convenience are acceptable    Describe stratified sampling, cluster sampling, systematic sampling, and voluntary response sampling    Distinguish between statistics and parameters      1 Background and Definitions  In the months leading up to an election, polls often tell us the percentages of voters that prefer each of the candidates. How do pollsters obtain this information?   Ideal case: ask the opinion of every registered voter.   Workable: contact a relatively small number of voters, and use the information from these voters to predict the preferences of the entire group of voters.  The process of polling requires two major steps:                 Statsitics is the study of     The polling problem is typical of a problem in statistics. We want some information about a large group of individuals, but we are able to collect information on only a small part of that group.  In statistical terminology, the large group is called a ,  and the part of the group on which we collect information is called a     A population is the of individuals about which information is sought. A sample is , containing the individuals that are actually observed.      Simple Random Sampling \\   A Simple Random Sample of size n is a sample chosen by a method in which collection of n population items is  to make up the sample just as in     Construct a simple random sample:  Using a computer random number generator. Suppose there are N items in the population. \\                   Using technology draw a simple random sample of five employees from the following list   a table of names fro 40 employees at a company       Solution       Samples of Convenience  In some cases, it is difficult or impossible to draw a sample in a truly random way.    a Sample of Convenince is a sample that is     Determine when samples of convenience are acceptable    A sample of convenience may be acceptable when it is reasonable to believe that systematic difference between the sample and the population    A sample of convenience may be not acceptable when it is reasonable to believe that systematic difference between the sample and the population     A construction engineer has just received a shipment of 1000 concrete blocks, each weighing approximately 50 pounds. The blocks have been delivered in a large pile. The engineer wishes to investigate the crushing strength of the blocks by measuring the strengths in a sample of 10 blocks. Explain why it might be difficult to draw a simple random sample of blocks. Describe how the engineer might draw a sample of convenience.    Solution     Other Sampling Methods     Statified Sampling   In stratified sampling, the population is divided into groups, called strata, where the members of each stratum are similar in some way. Then a simple random sample is drawn from each stratum. Stratified sampling is useful when the strata differ from one another, but the individuals within a stratum tend to be alike.   A company has 1000 employees, of whom 800 are full-time and 200 are part-time. The company wants to survey 50 employees about their opinions regarding benefits. Attitudes toward benefits may differ considerably between full-time and part-time employees. Why might it be a good idea to draw a stratified sample? Describe how one might be drawn.    Solution      Cluster Sampling   In cluster sampling, items are drawn from the population in groups, or clusters. Cluster sampling is useful when the population is too large and spread out for simple random sampling to be feasible. Cluster sampling is used extensively by U.S. government agencies in sampling the U.S. population to measure sociological factors such as income and unemployment.   To estimate the unemployment rate in a county, a government agency draws a simple random sample of households in the county. Someone visits each household and asks how many adults live in the household, and how many of them are unemployed. What are the clusters? Why is this a cluster sample?    Solution      Systematic Sampling   Imagine walking alongside a line of people and choosing every third one. That would produce a systematic sample. In a systematic sample, the population items are ordered. It is decided how frequently to sample items; for example, one could sample every third item, or every fifth item, or every hundredth item. Let k represent the sampling frequency. To begin the sampling, choose a starting place at random. Select the item in the starting place, along with every kth item after that.  Systematic sampling is sometimes used to sample products as they come off an assembly line, in order to check that they meet quality standards.  Automobiles are coming off an assembly line. It is decided to draw a systematic sample for a detailed check of the steering system. The starting point will be the third car, then every fifth car after that will be sampled. Which cars will be sampled?   Solution      Voluntary Response Sampling   Voluntary response samples are often used by the media to try to engage the audience. For example, a news commentator will invite people to tweet an opinion, or a radio announcer will invite people to call the station to say what they think. How reliable are voluntary response samples? To put it simply, {\\bf voluntary response samples are never reliable}. People who go to the trouble to volunteer an opinion tend to have stronger opinions than is typical of the population. In addition, people with negative opinions are often more likely to volunteer their responses than those with positive opinions.     An image of 36 individuals in 3 even rows where random people are boxed in to show simple random sampling    An image of 36 individuals in 3 even rows where every second person is boxed in to show systematic random sampling    An image of 36 individuals in 3 even rows where every group of six people have the same color outfits and 2 people are randomly picked from each sub group to show stratified random sampling    An image of 36 individuals in 3 even rows where every group of six people have the same color outfits and 2 subgroups are randomly picked to show cluster random sampling    "
 },
 {
-  "id": "notes-week-01-3",
+  "id": "week01-3",
   "level": "2",
-  "url": "notes-week-01.html#notes-week-01-3",
+  "url": "week01.html#week01-3",
   "type": "Objectives",
   "number": "",
   "title": "",
@@ -39,7 +39,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-Statistics",
   "level": "2",
-  "url": "notes-week-01.html#def-Statistics",
+  "url": "week01.html#def-Statistics",
   "type": "Definition",
   "number": "1",
   "title": "",
@@ -48,7 +48,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-Population_and_Sample",
   "level": "2",
-  "url": "notes-week-01.html#def-Population_and_Sample",
+  "url": "week01.html#def-Population_and_Sample",
   "type": "Definition",
   "number": "2",
   "title": "",
@@ -57,11 +57,65 @@ var ptx_lunr_docs = [
 {
   "id": "def-SRS",
   "level": "2",
-  "url": "notes-week-01.html#def-SRS",
+  "url": "week01.html#def-SRS",
   "type": "Definition",
   "number": "3",
   "title": "",
   "body": "  A Simple Random Sample of size n is a sample chosen by a method in which collection of n population items is  to make up the sample just as in    "
+},
+{
+  "id": "subsec-SRS-6",
+  "level": "2",
+  "url": "week01.html#subsec-SRS-6",
+  "type": "Example",
+  "number": "4",
+  "title": "",
+  "body": " Using technology draw a simple random sample of five employees from the following list   a table of names fro 40 employees at a company   "
+},
+{
+  "id": "def-SoC",
+  "level": "2",
+  "url": "week01.html#def-SoC",
+  "type": "Definition",
+  "number": "5",
+  "title": "",
+  "body": "  a Sample of Convenince is a sample that is    "
+},
+{
+  "id": "subsec-Samples_of_Convenience-6",
+  "level": "2",
+  "url": "week01.html#subsec-Samples_of_Convenience-6",
+  "type": "Example",
+  "number": "6",
+  "title": "",
+  "body": " A construction engineer has just received a shipment of 1000 concrete blocks, each weighing approximately 50 pounds. The blocks have been delivered in a large pile. The engineer wishes to investigate the crushing strength of the blocks by measuring the strengths in a sample of 10 blocks. Explain why it might be difficult to draw a simple random sample of blocks. Describe how the engineer might draw a sample of convenience.  "
+},
+{
+  "id": "sec-Other_Sample_methods-2-1-3",
+  "level": "2",
+  "url": "week01.html#sec-Other_Sample_methods-2-1-3",
+  "type": "Example",
+  "number": "7",
+  "title": "",
+  "body": " A company has 1000 employees, of whom 800 are full-time and 200 are part-time. The company wants to survey 50 employees about their opinions regarding benefits. Attitudes toward benefits may differ considerably between full-time and part-time employees. Why might it be a good idea to draw a stratified sample? Describe how one might be drawn.  "
+},
+{
+  "id": "sec-Other_Sample_methods-2-2-3",
+  "level": "2",
+  "url": "week01.html#sec-Other_Sample_methods-2-2-3",
+  "type": "Example",
+  "number": "8",
+  "title": "",
+  "body": " To estimate the unemployment rate in a county, a government agency draws a simple random sample of households in the county. Someone visits each household and asks how many adults live in the household, and how many of them are unemployed. What are the clusters? Why is this a cluster sample?  "
+},
+{
+  "id": "sec-Other_Sample_methods-2-3-4",
+  "level": "2",
+  "url": "week01.html#sec-Other_Sample_methods-2-3-4",
+  "type": "Example",
+  "number": "9",
+  "title": "",
+  "body": "Automobiles are coming off an assembly line. It is decided to draw a systematic sample for a detailed check of the steering system. The starting point will be the third car, then every fifth car after that will be sampled. Which cars will be sampled? "
 },
 {
   "id": "notes-week-02",
